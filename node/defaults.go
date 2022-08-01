0b0d52c8f3da99f54550a6ff40b8afe6d9c0b19c
Juan Pablo Abadia Medina <88802339+juanpabloabadiamedina@users.noreply.github.com>
2022-08-01 17:58:09 -0500

    Update defaults.go

--- node/defaults.go
+++ node/defaults.go
@@ -74,19 +74,19 @@ func DefaultDataDir() string {
 	if home != "" {
 		switch runtime.GOOS {
 		case "darwin":
-			return filepath.Join(home, "Library", "Ethereum")
+			return filepath.Join(home, "Library", "pad")
 		case "windows":
 			// We used to put everything in %HOME%\AppData\Roaming, but this caused
 			// problems with non-typical setups. If this fallback location exists and
 			// is non-empty, use it, otherwise DTRT and check %LOCALAPPDATA%.
-			fallback := filepath.Join(home, "AppData", "Roaming", "Ethereum")
+			fallback := filepath.Join(home, "AppData", "Roaming", "pad")
 			appdata := windowsAppData()
 			if appdata == "" || isNonEmptyDir(fallback) {
 				return fallback
 			}
-			return filepath.Join(appdata, "Ethereum")
+			return filepath.Join(appdata, "pad")
 		default:
-			return filepath.Join(home, ".ethereum")
+			return filepath.Join(home, ".pad")
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later
